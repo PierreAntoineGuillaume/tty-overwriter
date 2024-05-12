6fd9c5149faa73a524534334bc5ff1e80afc7f84
Pierre-Antoine Guillaume <pierreantoine.guillaume@gmail.com>
2024-05-12 22:33:10 +0200

    feat: body overwrite

--- examples/replace-body.rs
+++ examples/replace-body.rs
@@ -1,41 +1,39 @@
-use ctrlc::set_handler;
 use std::thread::sleep;
 use std::time::{Duration, SystemTime};
 use tty_overwriter::prelude::*;
 
 const AWAIT_TIME: Duration = Duration::from_millis(5);
 
-fn do_print(spinner: &Spinner) {
+fn do_print(spinner: &Spinner) -> String {
     let char = spinner.current();
-    println!("fmt             cargo fmt {char}");
-    println!("clippy          cargo clippy {char}");
-    println!("node modules    npm install {char}");
-    println!("analyse         long process {char}");
+    format!(
+        "fmt             cargo fmt {char}
+clippy          cargo clippy {char}
+node modules    npm install {char}
+analyse         long string {} {char}",
+        " ".repeat(80)
+    )
 }
 
-fn redraw(spinner: &Spinner) {
-    let looper = spinner.current();
-    let first = Movement::new().up(4).right(26);
-    let second = Movement::new().down(1).right(2);
-    let third = Movement::new().down(1).left(2);
-    let fourth = Movement::new().down(1);
-    let last = AnsiSeq::MoveLines { up: 0, down: 1 };
-    print!("{first}{looper}{second}{looper}{third}{looper}{fourth}{looper}{last}");
-}
-
-fn main() {
+fn main() -> std::io::Result<()> {
     let mut last_occurence = SystemTime::now();
     let mut spinner = Spinner::default();
-    do_print(&spinner);
     let hide = AnsiSeq::ShowAndHideCursor { show: false };
     print!("{hide}");
-    let _ = set_handler(|| {
-        let show = AnsiSeq::ShowAndHideCursor { show: true };
-        print!("{show}");
+    let _ = ctrlc::set_handler(|| {
+        print!(
+            "{}{}",
+            AnsiSeq::ShowAndHideCursor { show: true },
+            AnsiSeq::MoveLines { down: 1, up: 0 }
+        );
         std::process::exit(0);
     });
+
+    let mut body = Body::default();
+
     loop {
-        redraw(&spinner);
+        let (width, _) = term_size::dimensions().expect("it has dimensions");
+        body.overwrite(&do_print(&spinner), &mut std::io::stdout(), width)?;
 
         let should_sleep_duration = AWAIT_TIME
             .checked_sub(last_occurence.elapsed().unwrap())
--- src/body.rs
+++ src/body.rs
@@ -1,34 +1,119 @@
+use crate::prelude::AnsiSeq;
 use std::io::Write;
 
 #[derive(Debug, Default)]
-pub struct Body
-where
-{
-    buffer: String,
+pub struct Body {
+    buffer: Vec<usize>,
 }
 
-impl Body
-{
-    pub fn overwrite<Writer>(&mut self, new_text: &str, write: &mut Writer)
-        -> std::io::Result<()>
-        where Writer: Write
+const CLEAR_TIL_EOL: AnsiSeq = AnsiSeq::ClearCursorToEndOfLine;
+const CLEAR_TIL_EOF: AnsiSeq = AnsiSeq::ClearCursorToEndOfScreen;
+const JUMP_AT_BEGINNING: AnsiSeq = AnsiSeq::AbsoluteMove { horizontal: 0 };
+
+impl Body {
+    pub fn overwrite<T, Writer>(&mut self, new_text: &T, write: &mut Writer, available_width: usize) -> std::io::Result<()>
+    where
+        Writer: Write,
+        T: ToString,
     {
-        write.write_all(new_text.as_bytes())?;
+        let new_text = new_text.to_string();
+        let mut next_buffer = if self.buffer.is_empty() {
+            vec![]
+        } else {
+            Vec::with_capacity(self.buffer.capacity())
+        };
+        let mut symbols = vec![];
+
+        if new_text.is_empty() {
+            write!(symbols, "{CLEAR_TIL_EOL}")?;
+            next_buffer.push(0);
+        } else {
+            for (line_no, line) in new_text.lines().enumerate() {
+                next_buffer.push(line.len());
+                if line_no > 0 {
+                    symbols.push(b'\n');
+                }
+                symbols.write_all(line.as_bytes())?;
+                write!(symbols, "{CLEAR_TIL_EOL}")?;
+            }
+        }
+
+        match self.guess_previous_body_height(available_width) {
+            1 => {
+                write!(write, "{JUMP_AT_BEGINNING}")?;
+            }
+            lines if lines > 1 => {
+                let movement = AnsiSeq::MoveLines {
+                    down: 0,
+                    up: (lines - 1) as u16,
+                };
+                write!(write, "{JUMP_AT_BEGINNING}{movement}")?;
+            }
+            _ => {}
+        }
+
+        write!(symbols, "{CLEAR_TIL_EOF}")?;
+        write.write_all(&symbols)?;
+        self.buffer = next_buffer;
         Ok(())
+    }
+
+    fn guess_previous_body_height(&self, available_width: usize) -> usize {
+        let mut lines = 0;
+        for line in &self.buffer {
+            lines+= line/available_width + 1;
+        }
+        lines
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::error::Error;
+    use std::fmt::Write;
 
     #[test]
-    fn initial() {
+    fn initial() -> Result<(), Box<dyn Error>> {
         let mut body = Body::default();
         let mut buf = Vec::new();
-        body.overwrite("content", &mut buf).unwrap();
+        body.overwrite(&"content", &mut buf, 80)?;
 
-        let string = String::from_utf8(buf).unwrap();
-        assert_eq!(string, "content");
+        let string = String::from_utf8(buf.clone())?;
+        assert_eq!("content\\e[0K\\e[0J", string);
+
+        body.overwrite(&"content + content", &mut buf, 80)?;
+        let string = String::from_utf8(buf.clone())?;
+        assert_eq!("content\\e[0K\\e[0J\\e[0Gcontent + content\\e[0K\\e[0J", string);
+
+        body.overwrite(&"none", &mut buf, 80)?;
+        let string = String::from_utf8(buf.clone())?;
+
+        let mut expected = String::new();
+        expected.write_str("content")?; // first content
+        expected.write_str("\\e[0K")?; // clear til EOL
+        expected.write_str("\\e[0J")?; // clear til EOF
+        expected.write_str("\\e[0G")?; // go far left
+        expected.write_str("content + content")?; // second content
+        expected.write_str("\\e[0K")?; // clear til EOL
+        expected.write_str("\\e[0J")?; // clear til EOF
+        expected.write_str("\\e[0G")?; // go far left
+        expected.write_str("none")?; // third content
+        expected.write_str("\\e[0K")?; // clear til EOL
+        expected.write_str("\\e[0J")?; // clear til EOF
+
+        assert_eq!(expected, string);
+
+        body.overwrite(&"", &mut buf, 80)?;
+
+        expected.write_str("\\e[0G")?; // go far left
+        expected.write_str("\\e[0K")?; // clear til EOL
+        expected.write_str("\\e[0J")?; // clear til EOF
+
+        let string = String::from_utf8(buf.clone())?;
+
+        assert_eq!(expected, string,);
+
+        Ok(())
     }
 }
